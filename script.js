4321edd6a91522053de542fc6f6a530b7d5bb5a8
Muhamad Oka Augusta <oka.august@gmail.com>
2022-05-27 23:48:46 +0700

    add one round of rock paper scissor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // make the bot that will output rock, paper, or scissors
 
 function computerPlay() {
-  const choice = ["rock", "paper", "scissor"];
+  const choice = ["rock", "paper", "scissors"];
   let randomizer = Math.floor(Math.random() * choice.length);
   return choice[randomizer];
 }
@@ -9,7 +9,7 @@ function computerPlay() {
 function playerPlay() {
   let playerInput = prompt("Rock, paper, or scissors?").toLowerCase();
   let playerSelection = playerInput;
-  if (playerSelection == "" || playerSelection == undefined) {
+  if (playerSelection == undefined || playerSelection == "") {
     alert("Canceled");
   } else if (
     playerSelection == "rock" ||
@@ -28,3 +28,33 @@ const computerSelection = computerPlay();
 
 console.log("You choose " + playerSelection);
 console.log("The bot choose " + computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    console.log("You choose " + playerSelection);
+    console.log("The bot choose " + computerSelection);
+    console.log("It's a draw");
+  } else if (
+    // Player winning logic
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    console.log("You choose " + playerSelection);
+    console.log("The bot choose " + computerSelection);
+    console.log("You Win! " + playerSelection + " beats " + computerSelection);
+  } else if (
+    // Player losing logic
+    (computerSelection == "paper" && playerSelection == "rock") ||
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper")
+  ) {
+    console.log("You choose " + playerSelection);
+    console.log("The bot choose " + computerSelection);
+    console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+  } else {
+    console.log("I don't know how you break it. Congrats I guess?");
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
