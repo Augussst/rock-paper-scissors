559fcaa7528fd5a548f2338e83e3d39547b96313
Muhamad Oka Augusta <oka.august@gmail.com>
2022-05-30 16:22:27 +0700

    rewrite the script

--- script.js
+++ script.js
@@ -1,60 +1,33 @@
 // make the bot that will output rock, paper, or scissors
-
 function computerPlay() {
-  const choice = ["rock", "paper", "scissors"];
+  let choice = ["rock", "paper", "scissors"];
   let randomizer = Math.floor(Math.random() * choice.length);
   return choice[randomizer];
 }
 
-function playerPlay() {
-  let playerInput = prompt("Rock, paper, or scissors?").toLowerCase();
-  let playerSelection = playerInput;
-  if (playerSelection == undefined || playerSelection == "") {
-    alert("Canceled");
-  } else if (
-    playerSelection == "rock" ||
-    playerSelection == "paper" ||
-    playerSelection == "scissors"
-  ) {
-    return playerSelection;
-  } else {
-    alert("You have to choose, rock, paper, or scissors. Try again.");
-    playerPlay();
-  }
-}
-
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
-
-console.log("You choose " + playerSelection);
-console.log("The bot choose " + computerSelection);
-
+// make a fucntion that plays one round of rps
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == computerSelection) {
-    console.log("You choose " + playerSelection);
-    console.log("The bot choose " + computerSelection);
-    console.log("It's a draw");
-  } else if (
-    // Player winning logic
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "paper")
+  if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    console.log("You choose " + playerSelection);
-    console.log("The bot choose " + computerSelection);
-    console.log("You Win! " + playerSelection + " beats " + computerSelection);
+    return "You Win! " + playerSelection + " beats " + computerSelection;
   } else if (
     // Player losing logic
-    (computerSelection == "paper" && playerSelection == "rock") ||
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper")
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "paper")
   ) {
-    console.log("You choose " + playerSelection);
-    console.log("The bot choose " + computerSelection);
-    console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-  } else {
-    console.log("I don't know how you break it. Congrats I guess?");
+    return "You Lose! " + computerSelection + " beats " + playerSelection;
+  } else if (playerSelection === computerSelection) {
+    return "It's a draw";
   }
 }
+
+const computerSelection = computerPlay();
+const playerSelection = "paper";
+
+// make a functoin that plays 5 round of rps (repeat playRound 5 times)
 
 console.log(playRound(playerSelection, computerSelection));
